3f2b0f4766a62cb257f6dbae2677dd0b02dd7445
Islam Asar <semoahmed166@gmail.com>
2026-05-11 03:01:03 +0300

    Changes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   // This explicitly allows your local network IP to access the dev server
   // without Next.js blocking the Hot Module Replacement (HMR) requests.
-  allowedDevOrigins: ['192.168.1.9'],
+  allowedDevOrigins: ['10.61.2.33'],
 };
 
 export default nextConfig;
